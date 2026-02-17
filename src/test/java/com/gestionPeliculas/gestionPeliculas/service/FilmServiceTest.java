0ee8c48206eaabdbf858db2d6313df3c7795fe4e
josepllopis <josepllopis33@gmail.com>
2026-02-18 00:18:43 +0100

    Solucionar problemas tests

--- src/test/java/com/gestionPeliculas/gestionPeliculas/service/FilmServiceTest.java
+++ src/test/java/com/gestionPeliculas/gestionPeliculas/service/FilmServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Date;
 import java.util.List;
@@ -25,6 +26,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 public class FilmServiceTest {
 
     @Mock
